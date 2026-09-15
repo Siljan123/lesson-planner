3682a4898be18c379e6f82dc390816481dcb6cd2
Siljan123 <jansilgasoc859@gmail.com>
2026-09-16 00:12:27 +0800

    fix

---
+++ app/components/documents/DocumentsTable.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// TODO: Implement Documents Table
+</script>
+
+<template>
+  <div class="rounded-md border p-8 text-center text-muted-foreground">
+    Documents table coming soon...
+  </div>
+</template>
